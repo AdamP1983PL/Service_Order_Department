63b693bf56c0891eb1294542fbedef3ca6c7ca2c
Adam Podlaski <podlaski83@gmail.com>
2024-03-17 17:52:44 +0100

    initila commit

---
+++ src/main/java/com/service_order/ServiceOrderApplication.java
@@ -0,0 +1,20 @@
+package com.service_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:database.properties")
+//Registering VehicleDepartment Service as a Eureka Client with the Eureka Server
+@EnableEurekaClient
+@EnableFeignClients
+public class ServiceOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOrderApplication.class, args);
+	}
+
+}

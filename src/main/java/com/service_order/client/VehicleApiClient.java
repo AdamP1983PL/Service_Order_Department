b75e74986233986402971c01e0f85025bc68cca4
Adam Podlaski <podlaski83@gmail.com>
2024-03-22 09:18:43 +0100

    test

--- src/main/java/com/service_order/client/VehicleApiClient.java
+++ src/main/java/com/service_order/client/VehicleApiClient.java
@@ -2,8 +2,6 @@ package com.service_order.client;
 
 import com.service_order.service.dto.vehicle_dto.VehicleDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,16 +10,16 @@ import java.util.List;
 @FeignClient(name = "VEHICLE-DEPARTMENT")
 public interface VehicleApiClient {
 
-    @GetMapping("/api/vehicle/")
+    @GetMapping("/vehicles/all-vehicles")
     List<VehicleDto> findAllVehicles();
 
-    @GetMapping("/api/vehicle/id/{id}")
+    @GetMapping("/vehicles/id/{id}")
     VehicleDto findVehicleById(@PathVariable("id") Long id);
 
-    @GetMapping("/api/vehicle/vin/{vin}")
+    @GetMapping("/vehicles/vin/{vin}")
     VehicleDto findVehicleByVIN(@PathVariable("vin") String vin);
 
-    @GetMapping("/api/vehicle/reg-num/{number}")
+    @GetMapping("/vehicles/reg-num/{number}")
     VehicleDto findVehicleByRegistrationNumber(@PathVariable("number") String number);
 
 }
